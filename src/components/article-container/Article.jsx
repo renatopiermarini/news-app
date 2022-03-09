3497b9cd0765aeb13b08384265b17b567b89ca47
Renato Piermarini <renatopiermarinih@gmail.com>
2022-03-09 14:56:35 -0300

    styling

--- src/components/article-container/Article.jsx
+++ src/components/article-container/Article.jsx
@@ -7,7 +7,7 @@ export const Article = ({ rights, author, title, summary, link, media }) => {
       {author && <p>{author}</p>}
       <h2>{title}</h2>
       {media && <img src={media} className="imagen" />}
-      <p>{summary}</p>
+      <p className="description">{summary}</p>
       {rights && <p>{"Rights: " + rights}</p>}
       <div className="button-div">
         <a href={link} target="_blank">
